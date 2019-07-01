29e5c92385bc44726dd09aca6d341cfc324d0212
Enes Usta <enesusta@email.com>
2019-07-01 14:18:26 +0300

    Event binding ( clickListener ) implementation learned!

--- src/app/CourseComponent.ts
+++ src/app/CourseComponent.ts
@@ -4,12 +4,17 @@ import {CoursesService} from './course/courses.service';
 @Component({
   selector: 'courses',
   template: `
-    <button class="btn btn-primary">Save</button>
+    <button class="btn btn-primary" [class.active]="isActive" (click)="onSave()">Save</button>
   `
 })
 export class CourseComponent {
+  isActive = true;
   title = 'List of courses';
   courses;
+
+  onSave() {
+    console.log('button was clicked');
+  }
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
