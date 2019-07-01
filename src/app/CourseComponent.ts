c380255f46891e85c929edafba5fc0cd94d73cad
Enes Usta <enesusta@email.com>
2019-07-01 13:14:40 +0300

    update

---
+++ src/app/CourseComponent.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'courses',
+  template: `
+    <h2>{{getTitle()}}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{course}}
+      </li>
+    </ul>
+  `
+})
+export class CourseComponent {
+  title = 'List of courses';
+  courses = ['course1', 'course2', 'course3'];
+
+  getTitle() {
+    return this.title;
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CourseComponent} from './CourseComponent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +17,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
