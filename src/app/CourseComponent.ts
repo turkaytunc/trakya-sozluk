fa7bd8685cf1df963ab265b6633512e22b5a72a0
Enes Usta <enesusta@email.com>
2019-07-03 17:16:53 +0300

    update

--- src/app/CourseComponent.ts
+++ src/app/CourseComponent.ts
@@ -4,64 +4,11 @@ import {CoursesService} from './course/courses.service';
 @Component({
   selector: 'courses',
   template: `
-    <h2>{{getTitle()}}</h2>
-    <ul>
-      <li *ngFor="let course of courses">
-        {{course}}
-      </li>
-    </ul>
-    <button class="btn btn-primary" [class.active]="isActive" (click)="onSave($event)">Save</button>
-    <div (click)="onDivClicked()">
-      <button (click)="onSave($event)">Save</button>
-    </div>
-    <input (keyup)="onKeyUp($event)">
-    <input (keyup.enter)="onKeyUp2($event)"/>
-    <input [value]="email" (keyup.enter)="onKeyUp3()"/>
-    <input [value]="email" (keyup.enter)=" email = $event.target.value; onKeyUp4()"/>
+    {{text | summary}}
   `
 })
 export class CourseComponent {
-  isActive = true;
-  title = 'List of courses';
-  courses;
-  email = 'enesusta@email.com';
 
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
-  }
+  text = 'adasuihfasuydgasfguyasasufgasfuygasuydgsauyfgasuydgsauyfgasuyfgasuydgasuyfgasdytasfasuydgasuyfgasuyfgasdtyasfgasuygasf';
 
-
-  onSave($event) {
-    $event.stopPropagation();
-    console.log('button was clicked' + $event);
-  }
-
-
-  getTitle() {
-    return this.title;
-  }
-
-  onDivClicked() {
-    console.log('div was clicked');
-  }
-
-  onKeyUp($event) {
-    if ($event.keyCode === 13) {
-      console.log('Enter was pressed');
-    }
-
-  }
-
-  onKeyUp2($event) {
-    console.log($event.target.value);
-  }
-
-
-  onKeyUp3() {
-    console.log(this.email);
-  }
-
-  onKeyUp4() {
-    return '';
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import {AppComponent} from './app.component';
 import {CourseComponent} from './CourseComponent';
 import {CoursesService} from './course/courses.service';
 import { ListComponent } from './list/list.component';
+import {FormsModule} from '@angular/forms';
+import {SummaryPipe} from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
-    ListComponent
+    ListComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     CoursesService
