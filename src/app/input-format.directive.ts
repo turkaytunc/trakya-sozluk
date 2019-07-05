cbff506113dc44f510129cdc0d15dcc67eb79532
Enes Usta <enesusta@email.com>
2019-07-05 11:09:20 +0300

    input-directive is modified as a format

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
 
-  constructor() { }
+  // tslint:disable-next-line:no-input-rename
+  @Input('format') format;
 
+  constructor(private el: ElementRef) {
+  }
+
+  @HostListener('blur') onBlur() {
+    const value: string = this.el.nativeElement.value;
+
+    if (this.format === 'lowercase') {
+      this.el.nativeElement.value = value.toLowerCase();
+    } else {
+      this.el.nativeElement.value = value.toUpperCase();
+    }
+
+  }
+  
 }
